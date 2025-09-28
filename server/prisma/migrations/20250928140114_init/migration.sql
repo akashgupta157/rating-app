d93f054bc2bb484b644803985229c45b5fcfea04
Akash Gupta <akashgupta989322@gmail.com>
2025-09-28 19:33:06 +0530

    update

---
+++ server/prisma/migrations/20250928140114_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `comment` on the `ratings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."ratings" DROP COLUMN "comment";
